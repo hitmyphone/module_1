5675812eca1e1afd634d68e5672ac6bf1a560541
hitmyphone <ulyshcka228@gmail.com>
2024-11-07 19:57:45 +0300

    Add files via upload

---
+++ stroki.py
@@ -0,0 +1,8 @@
+example = "derevo"
+print("Первый символ: "+example[0])
+print("Последний символ: "+example[-1])
+print("Вторая половина: "+example[3:])
+print("Наоборот: "+example[::-1])
+print("Каждый второй: "+example[1::2])
+
+
