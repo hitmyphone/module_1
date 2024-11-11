b8fa8659cc15e6644870d649279acd2aa9b2d595
hitmyphone <ulyshcka228@gmail.com>
2024-11-11 18:22:35 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = tuple([1,2, True, "string"])
+print(immutable_var)
+
+#immutable_var[0]= 3 #команда не выполнится, тк кортежи не поддерживают обращение к отдельному элементу
+
+mutable_list = [1,2,True,"string"]
+mutable_list [0] = 3
+print(mutable_list)
+
